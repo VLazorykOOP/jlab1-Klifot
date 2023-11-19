e4fc94f400ff9c42eee6fcabecded54686d249c4
Asmareal <Asmareal@DESKTOP-58FEAKF>
2023-11-19 20:37:25 +0200

    first save

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,27 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Lab 1 Java");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите значение x: ");
+        double x = scanner.nextDouble();
+
+        System.out.print("Введите значение y: ");
+        double y = scanner.nextDouble();
+
+        double numerator = x * y + Math.pow(x + y, 3);
+        double denominator = Math.pow(x, 2) * Math.pow(y, 2) * (x - y);
+
+        int result;
+        if (denominator == 0) {
+            System.out.println("Знаменатель равен нулю, деление на ноль невозможно.");
+        } else {
+            double resultDouble = numerator / denominator;
+            result = (int) resultDouble; // Преобразование результата в целое число.
+            System.out.println("Результат: " + result);
+        }
+
+        scanner.close(); // Закрываем Scanner после использования.
     }
 }
